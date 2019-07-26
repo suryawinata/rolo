016d8077f67d9c6fde353d9b102c269ac26c7b84
Surya Winata <s.winata@accenture.com>
2019-07-26 13:23:19 +1000

    Updates

---
+++ precache-manifest.09c2d524f5c13c5af0a125e377539433.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00acb27f5d0f88a91c635e83ba027c5b",
+    "url": "/rolo/index.html"
+  },
+  {
+    "revision": "be26e4f4b637b2a40b7b",
+    "url": "/rolo/static/css/main.ffbf457d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/rolo/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "be26e4f4b637b2a40b7b",
+    "url": "/rolo/static/js/main.bee0c651.chunk.js"
+  },
+  {
+    "revision": "776c5d156cfc8bdf5eb3",
+    "url": "/rolo/static/js/runtime~main.87b16e84.js"
+  }
+]);
